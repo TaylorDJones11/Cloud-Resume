864845ea7d5a505c4d331d68a387fa6361bce5a8
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-12-27 17:34:38 -0600

    adding scripts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Counter
 const counter = document.querySelector('.counter');
 async function updateCounter() {
-  const response = await fetch('/api/counter');
+  const response = await fetch(
+    'https://oaczpt32rgouh5p4ztk3d37xo40jwpbr.lambda-url.us-east-1.on.aws/'
+  );
   const data = await response.json();
   counter.textContent = `Views: ${data.views}`;
 }
